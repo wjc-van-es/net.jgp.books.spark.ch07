7b95e7620744eadcf41ac5e8d18d4e44e800716e
Willem van Es <wjc.van.es@gmail.com>
2021-07-31 17:27:15 +0200

    Commenting the csv ingestion code examples

--- src/main/java/net/jgp/books/spark/ch07/lab200_csv_ingestion/ComplexCsvToDataframeApp.java
+++ src/main/java/net/jgp/books/spark/ch07/lab200_csv_ingestion/ComplexCsvToDataframeApp.java
@@ -6,49 +6,49 @@ import org.apache.spark.sql.SparkSession;
 
 /**
  * CSV ingestion in a dataframe.
- * 
+ *
  * @author jgp
  */
 public class ComplexCsvToDataframeApp {
 
-  /**
-   * main() is your entry point to the application.
-   * 
-   * @param args
-   */
-  public static void main(String[] args) {
-    ComplexCsvToDataframeApp app = new ComplexCsvToDataframeApp();
-    app.start();
-  }
+    /**
+     * main() is your entry point to the application.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ComplexCsvToDataframeApp app = new ComplexCsvToDataframeApp();
+        app.start();
+    }
 
-  /**
-   * The processing code.
-   */
-  private void start() {
-    // Creates a session on a local master
-    SparkSession spark = SparkSession.builder()
-        .appName("Complex CSV to Dataframe")
-        .master("local")
-        .getOrCreate();
-    System.out.println("Using Apache Spark v" + spark.version());
+    /**
+     * The processing code.
+     */
+    private void start() {
+        // Creates a session on a local master
+        SparkSession spark = SparkSession.builder()
+                .appName("Complex CSV to Dataframe")
+                .master("local")
+                .getOrCreate();
+        System.out.println("Using Apache Spark v" + spark.version());
 
-    // Reads a CSV file with header, called books.csv, stores it in a
-    // dataframe
-    Dataset<Row> df = spark.read().format("csv")
-        .option("header", "true")
-        .option("multiline", true)
-        .option("sep", ";")
-        .option("quote", "*")
-        .option("dateFormat", "MM/dd/yyyy")
-        .option("inferSchema", true)
-        .load("data/books.csv");
+        // Reads a CSV file with header, called books.csv, stores it in a
+        // dataframe
+        Dataset<Row> df = spark.read().format("csv")
+                .option("header", "true")
+                .option("multiline", true)
+                .option("sep", ";") // primary field separator character
+                .option("quote", "*") //Indicating a second field separator to escape any primary separator characters inside
+                .option("dateFormat", "MM/dd/yyyy")
+                .option("inferSchema", true) //try to infer the schema
+                .load("data/books.csv");
 
-    System.out.println("Excerpt of the dataframe content:");
+        System.out.println("Excerpt of the dataframe content:");
 
-    // Shows at most 7 rows from the dataframe, with columns as wide as 70
-    // characters
-    df.show(7, 70);
-    System.out.println("Dataframe's schema:");
-    df.printSchema();
-  }
+        // Shows at most 7 rows from the dataframe, with columns as wide as 70
+        // characters
+        df.show(7, 180);
+        System.out.println("Dataframe's schema:");
+        df.printSchema();
+    }
 }
--- src/main/java/net/jgp/books/spark/ch07/lab201_csv_ingestion_push_down/ComplexCsvToDataframePushDownFilterApp.java
+++ src/main/java/net/jgp/books/spark/ch07/lab201_csv_ingestion_push_down/ComplexCsvToDataframePushDownFilterApp.java
@@ -6,51 +6,53 @@ import org.apache.spark.sql.SparkSession;
 
 /**
  * CSV ingestion in a dataframe.
- * 
+ *
  * @author jgp
  */
 public class ComplexCsvToDataframePushDownFilterApp {
 
-  /**
-   * main() is your entry point to the application.
-   * 
-   * @param args
-   */
-  public static void main(String[] args) {
-    ComplexCsvToDataframePushDownFilterApp app =
-        new ComplexCsvToDataframePushDownFilterApp();
-    app.start();
-  }
+    /**
+     * main() is your entry point to the application.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ComplexCsvToDataframePushDownFilterApp app =
+                new ComplexCsvToDataframePushDownFilterApp();
+        app.start();
+    }
 
-  /**
-   * The processing code.
-   */
-  private void start() {
-    // Creates a session on a local master
-    SparkSession spark = SparkSession.builder()
-        .appName("Complex CSV to Dataframe")
-        .master("local")
-        .getOrCreate();
-    System.out.println("Using Apache Spark v" + spark.version());
+    /**
+     * The processing code.
+     */
+    private void start() {
+        // Creates a session on a local master
+        SparkSession spark = SparkSession.builder()
+                .appName("Complex CSV to Dataframe")
+                .master("local")
+                .getOrCreate();
+        System.out.println("Using Apache Spark v" + spark.version());
 
-    // Reads a CSV file with header, called books.csv, stores it in a
-    // dataframe
-    Dataset<Row> df = spark.read().format("csv")
-        .option("header", "true")
-        .option("multiline", true)
-        .option("sep", ";")
-        .option("quote", "*")
-        .option("dateFormat", "MM/dd/yyyy")
-        .option("inferSchema", true)
-        .load("data/books.csv")
-        .filter("authorId = 1");
+        // Reads a CSV file with header, called books.csv, stores it in a
+        // dataframe
+        Dataset<Row> df = spark.read().format("csv")
+                .option("header", "true")
+                .option("multiline", true)
+                .option("sep", ";")
+                .option("quote", "*")
+                .option("dateFormat", "MM/dd/yyyy")
+                .option("inferSchema", true)
+                .load("data/books.csv")
+                //.filter("authorId = 1") //filtering JK Rowling
+                .filter("authorId = 2") //filtering JK Rowling
+                ;
 
-    System.out.println("Excerpt of the dataframe content:");
+        System.out.println("Excerpt of the dataframe content:");
 
-    // Shows at most 7 rows from the dataframe, with columns as wide as 90
-    // characters
-    df.show(7, 70);
-    System.out.println("Dataframe's schema:");
-    df.printSchema();
-  }
+        // Shows at most 7 rows from the dataframe, with columns as wide as 110 characters
+        // with truncate after 110, only the title field of the second record gets truncated
+        df.show(7, 110);
+        System.out.println("Dataframe's schema:");
+        df.printSchema();
+    }
 }
